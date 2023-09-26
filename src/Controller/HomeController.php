86d50a0d06390464a74e7aa86be48d0abbf209c9
ebusy9 <g404ebus@gmail.com>
2023-09-26 15:02:50 +0200

    controller ajouté, templates twig fini

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/company', name: 'app_company')]
+    public function company(): Response
+    {
+        return $this->render('home/company.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/jobs', name: 'app_jobs')]
+    public function jobs(): Response
+    {
+        return $this->render('home/jobs.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/jobs/show', name: 'app_jobs_show')]
+    public function jobsShow(): Response
+    {
+        return $this->render('home/jobs_show.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('home/login.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('home/profile.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(): Response
+    {
+        return $this->render('home/register.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
